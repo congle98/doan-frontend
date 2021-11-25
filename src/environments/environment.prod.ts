5206f37c4c24b5824bca3197bda09e37a669497b
cong98 <lethanhcongcnhn@gmail.com>
2021-11-25 12:28:43 +0700

    oke

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  baseUrl: "http://localhost:8080",
+  firebaseConfig: {
+    apiKey: "AIzaSyDGFUzjdEupKQizUZluVqF5NZxMftGQn0A",
+    authDomain: "doan-shopping.firebaseapp.com",
+    projectId: "doan-shopping",
+    storageBucket: "doan-shopping.appspot.com",
+    messagingSenderId: "873365734741",
+    appId: "1:873365734741:web:a4de3789dd9fd3b954dbdc",
+    measurementId: "G-3X60EDMLGM"
+  }
 };
